29486a868e13d783a654c463e849b214964ddcca
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-02 00:09:30 +0100

    update the logic for image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,10 +6,10 @@ const Hero = () => {
     <>
      <section className='hero__container'>
         {trending.map((item) => {
-            const {id, img, altText, topic, text} = item
+            const {id, img, mobile, altText, topic, text} = item
             return <div key={id} className='hero__wrapper'>
-                <img src={img} alt={altText} />
-                <div>
+                <img src={window.innerWidth < 800 ? mobile : img} alt={altText} />
+                <div className='hero__info'>
                     <h1>{topic}</h1>
                     <div>
                         <p>{text}</p>
