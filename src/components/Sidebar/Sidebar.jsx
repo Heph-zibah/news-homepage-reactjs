eb67ea18b1edd5cc9e1497615e2fa767c4c64350
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-02 00:07:57 +0100

    added the h1 tag

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,10 +4,11 @@ import {newNews} from '../Data'
 const Sidebar = () => {
   return (
     <>
-        <aside>
+        <aside className='sidebar__container'>
+          <h1>New</h1>
             {newNews.map((item) => {
                 const {id, header,text} = item
-                return <div key={id}>
+                return <div key={id} className='sidebar__wrapper'>
                     <h4>{header}</h4>
                     <p>{text}</p>
                 </div>
