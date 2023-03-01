dd56db8d7564b3b1a7af712c8d5a5899719ad840
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-01 20:27:32 +0100

    added the footer tag

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,13 @@ function App() {
     <>
       <div className="container">
         <Navbar/>
+        <footer>
+          <p>
+            Challenge by <a href="#">
+            Challenge by Frontend Mentor. Coded by Your Name Here.</a>. 
+            Coded by <a href="#">Oluwatosin Abigail Daramola</a>.
+          </p>
+        </footer>
       </div>
     </>
   );
