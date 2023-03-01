1d0c3ed93055c370557a36a77709ecc48347cfff
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-01 11:51:51 +0100

    created component folder and added navbar folder

---
+++ src/components/Data.js
@@ -0,0 +1,27 @@
+export const navLinks = [
+    {
+        id:1,
+        url: '/home',
+        text: 'home'
+    },
+    {
+        id:1,
+        url: '/new',
+        text: 'new'
+    },
+    {
+        id:1,
+        url: '/popular',
+        text: 'popular'
+    },
+    {
+        id:1,
+        url: '/trending',
+        text: 'trending'
+    },
+    {
+        id:1,
+        url: '/categories',
+        text: 'categories'
+    },
+]
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import logo from '../../assets/images/logo.svg'
+import {VscMenu} from 'react-icons/vsc'
+import { navLinks } from '../Data'
+
+const Navbar = () => {
+  return (
+    <>
+        <header>
+            <div className="header__logo">
+                <img src={logo} alt="company logo" />
+            </div>
+            <nav>
+                <ul>
+                    {navLinks.map((navlink) => {
+                        const {id, url, text} = navlink
+                        return <li key={id}>{text}</li>
+                    })}
+                </ul>
+            </nav>
+            <div className="mobile__menu">
+                <VscMenu/>
+            </div>
+        </header>
+    </>
+  )
+}
+
+export default Navbar
